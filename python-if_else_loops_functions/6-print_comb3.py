19ead335812402d0aaa3ae1b92545501dd63d8b7
dangjose <dangjos02@gmail.com>
2025-05-20 20:45:23 +1000

    print comb3

---
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+for i in range(10):
+    for j in range(10):
+        if i == j or i > j:
+            continue
+        else:
+            print('{}'.format(i), end='')
+            if i == 8 and j == 9:
+                print('{}'.format(j))
+            else:
+                print('{}'.format(j), end=', ')
