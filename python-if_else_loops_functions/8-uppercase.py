d67938b259696e16012e1ba97b712a2d8f3ad475
dangjose <dangjos02@gmail.com>
2025-05-22 13:44:10 +1000

    uppercase

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    conv_str = ''
+    for c in str:
+        if 97 <= ord(c) <= 122:
+            conv_str += chr(ord(c) - 32)
+        else:
+            conv_str += c
+    print(conv_str)
