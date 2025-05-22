ca394faab9b8bea4d7cca4d08205bb4091609247
dangjose <dangjos02@gmail.com>
2025-05-22 16:45:03 +1000

    1-calculation.py

---
+++ python-import_modules/1-calculation.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import calculator_1 as calc
+
+if __name__ == "__main__":
+    a = 10
+    b = 5
+
+    calc.add(a, b)
+    calc.sub(a, b)
+    calc.mul(a, b)
+    calc.div(a, b)
