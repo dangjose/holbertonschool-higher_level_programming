a858e2be5a28a39623c97171743d6ec9bf4f8416
dangjose <dangjos02@gmail.com>
2025-05-22 13:58:57 +1000

    remove new line

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -3,9 +3,9 @@ def print_last_digit(number):
     if number < 0:
         abs_num = number * -1
         last_dig = abs_num % 10
-        print(last_dig)
+        print(last_dig, end='')
         return (last_dig)
     else:
         last_dig = number % 10
-        print(last_dig)
+        print(last_dig, end='')
         return (last_dig)
