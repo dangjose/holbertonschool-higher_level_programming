511bf79f8e74ceff9a062f9183dfc23e51907165
dangjose <dangjos02@gmail.com>
2025-05-27 14:39:59 +1000

    fix negatve value

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -3,7 +3,7 @@ import random
 number = random.randint(-10000, 10000)
 if number < 0:
     abs_num = number * -1
-    last_dig = abs_num % -10
+    last_dig = (abs_num % 10) * -1
 else:
     last_dig = (number % 10)
 if last_dig > 5:
