cf2d132a17b30656db992afe75a2b9afd3dd7559
dangjose <dangjos02@gmail.com>
2025-05-31 16:28:17 +1000

    string format

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
     for nums in my_list:
-        print(nums)
-
+        print('{}'.format(nums))
 my_list = [1, 2, 3, 4, 5]
 print_list_integer(my_list)
