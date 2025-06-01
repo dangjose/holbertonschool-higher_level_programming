610b3e251912ab53808d6ff50b16f869f96575f6
dangjose <dangjos02@gmail.com>
2025-06-01 15:56:54 +1000

    format integers

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -4,7 +4,7 @@ def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for idx, num in enumerate(row):
             if idx == len(row) - 1:
-                print('{}'.format(num), end='')
+                print('{:d}'.format(num), end='')
             else:
-                print('{}'.format(num), end=' ')
+                print('{:d}'.format(num), end=' ')
         print()
