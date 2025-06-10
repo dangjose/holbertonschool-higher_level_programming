90c2bcc5e825b3eadab3d389dfc94290d1b7586d
dangjose <dangjos02@gmail.com>
2025-06-10 17:52:54 +1000

    only raise IndexError

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -5,10 +5,12 @@ def safe_print_list_integers(my_list=[], x=0):
 
     for i in range(x):
         try:
-                print('{:d}'.format(my_list[i]), end='')
-                count += 1
-        except (TypeError, IndexError, ValueError) as e:
+            print('{:d}'.format(my_list[i]), end='')
+            count += 1
+        except IndexError as e:
             raise e
+        except (TypeError, ValueError):
+            break
     print()
 
     return count
