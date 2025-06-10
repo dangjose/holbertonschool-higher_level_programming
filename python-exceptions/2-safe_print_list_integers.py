2b870a0fa7254d276c98416fc62590cfd88049cd
dangjose <dangjos02@gmail.com>
2025-06-10 17:36:55 +1000

    unindent print newline

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -9,6 +9,6 @@ def safe_print_list_integers(my_list=[], x=0):
                 count += 1
         except (TypeError, IndexError, ValueError):
             break
-        print()
+    print()
 
     return count
