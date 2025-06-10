ea9175324391ee59a7c5283ed3c53aeee9818b3b
dangjose <dangjos02@gmail.com>
2025-06-10 17:00:14 +1000

    pycodestyle

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -4,17 +4,17 @@ def roman_to_int(roman_string):
     if not isinstance(roman_string, str):
         return 0
 
-    roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    r_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
 
     total = 0
     last_value = 0
 
     for char in reversed(roman_string):
-        value = roman_dict[char]
+        value = r_dict[char]
         if value < last_value:
             total -= value
         else:
             total += value
-        last_value = roman_dict[char]
+        last_value = r_dict[char]
 
     return total
