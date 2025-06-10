440e833faca599e168acc7e0e5e554edae0cb348
dangjose <dangjos02@gmail.com>
2025-06-10 16:09:00 +1000

    10-best_score.py

---
+++ python-more_data_structures/10-best_score.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def best_score(a_dictionary):
+    if not a_dictionary:
+        return None
+
+    current_hs = list(a_dictionary.values())[0]
+    score_key = None
+    for key in a_dictionary:
+        if current_hs < a_dictionary[key]:
+            current_hs = a_dictionary.get(key)
+            score_key = key
+    return score_key
