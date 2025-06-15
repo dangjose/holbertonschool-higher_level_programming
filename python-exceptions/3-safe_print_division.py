6c9defe210fba4bfac8bed6447e7633cb9b2c4b4
dangjose <dangjos02@gmail.com>
2025-06-15 10:55:00 +1000

    handle ZeroDivisionError

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
 
 def safe_print_division(a, b):
+    result = None
     try:
         result = a / b
-    except (TypeError, ValueError, IndexError, ZeroDivisionError) as e:
+    except (TypeError, ValueError, IndexError) as e:
         print(e)
+    except ZeroDivisionError:
+        pass
     finally:
-        print("Inside result: {:.1f}".format(result))
+        if result is not None:
+            print("Inside result: {:.1f}".format(result))
+        else:
+            print("Inside result: {}".format(result))
         return result
