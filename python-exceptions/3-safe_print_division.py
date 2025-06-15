c138361c9de84696d98dd7ec884a99b5d3bd5b52
dangjose <dangjos02@gmail.com>
2025-06-15 10:46:55 +1000

    float instead of int

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -6,4 +6,4 @@ def safe_print_division(a, b):
     except (TypeError, ValueError, IndexError, ZeroDivisionError) as e:
         print(e)
     finally:
-        print("Inside result: {:d}".format(result))
+        print("Inside result: {:f}".format(result))
