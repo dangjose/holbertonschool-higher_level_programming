a84732c5faf8c38e060fc97c027d46ccad798deb
dangjose <dangjos02@gmail.com>
2025-06-17 16:35:05 +1000

    1-square.py

---
+++ python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Task 1. Square with size"""
+
+
+class Square:
+    """
+        Defines a square
+
+        Args:
+            None
+
+        Returns:
+            Nothing
+    """
+    __size = None
