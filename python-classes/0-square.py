3350c4b6c1f2ad7ab2f219b8bf75d2e53994424e
dangjose <dangjos02@gmail.com>
2025-06-17 12:20:35 +1000

    0-square.py

---
+++ python-classes/0-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Task 0. My first square"""
+
+
+class Square:
+    """
+        Empty class
+
+        Args:
+            None
+
+        Returns:
+            Nothing
+    """
+    pass
