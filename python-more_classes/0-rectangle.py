0757fd83e6dd11ff802f69853ec53c4fa3fe05c8
dangjose <dangjos02@gmail.com>
2025-06-17 18:20:05 +1000

    0-rectangle.py

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Task 0. Simple rectangle"""
+
+
+class Rectangle:
+    """
+        Empty class
+
+        Args:
+            None
+
+        Returns:
+            Nothing
+    """
+    pass
