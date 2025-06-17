c69471edd5f57da2d80179c6d23f4ebcf2114771
dangjose <dangjos02@gmail.com>
2025-06-17 17:57:59 +1000

    fill with spaces

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -62,4 +62,7 @@ class Square:
             for i in range(self.__position[1]):
                 print()
             for i in range(self.__size):
-                print('_' * self.__position[0] + '#' * self.__size)
+                if self.__position[1] > 0:
+                    print('_' * self.__position[0] + '#' * self.__size)
+                else:
+                    print(' ' * self.__position[0] + '#' * self.__size)
