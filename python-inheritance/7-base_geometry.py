eb13c9ce87400bf89673f88acd4511269f595472
dangjose <dangjos02@gmail.com>
2025-06-21 12:32:06 +1000

    7-base_geometry.py

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Task 7. Integer validator"""
+
+
+class BaseGeometry():
+    """
+        Args:
+            None
+    """
+
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validate integer"""
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
