e53cf9b70cce711ed347d395e30492aebaad3ad7
dangjose <dangjos02@gmail.com>
2025-06-21 12:13:49 +1000

    6-base_geometry.py

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Task 6. Improve Geometry"""
+
+
+class BaseGeometry():
+    """
+        Args:
+            None
+    """
+
+    def area(self):
+        raise Exception("area() is not implemented")
