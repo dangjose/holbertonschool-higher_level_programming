92c23af8cebf42df204eaa019f6683880ea2ab8f
dangjose <dangjos02@gmail.com>
2025-06-24 15:26:21 +1000

    8-class_to_json.py

---
+++ python-input_output/8-class_to_json.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Task 8. Class to JSON"""
+
+import json
+
+
+def class_to_json(obj):
+    '''
+        Returns the dictionary description of an object
+        for JSON serialization.
+
+        Args:
+            obj: An instance of a class
+
+        Returns:
+            A dictionary of serializable attributes
+    '''
+    return object.__dict__
