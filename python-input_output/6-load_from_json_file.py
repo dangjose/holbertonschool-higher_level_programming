97fc7900a3a142ef9efd813ac5c6600e38b68ce2
dangjose <dangjos02@gmail.com>
2025-06-24 14:44:12 +1000

    6-load_from_json_file.py

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Task 6. Create object from a JSON file"""
+
+import json
+
+
+def load_from_json_file(filename):
+    '''
+        Creates an Object from a “JSON file”
+
+        Args:
+            filename: JSON file.
+
+        Returns:
+            Nothing
+    '''
+    with open(filename, 'r') as f:
+        data = json.load(f)
