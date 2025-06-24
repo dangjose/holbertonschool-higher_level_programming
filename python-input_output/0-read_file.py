e50580de4791fc28d79104c6153e414c47a12803
dangjose <dangjos02@gmail.com>
2025-06-24 12:43:07 +1000

    documentation

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -2,6 +2,15 @@
 """Task 0. Read file"""
 
 def read_file(filename=""):
+    '''
+        Reads a text file and print to stdout
+
+        Args:
+            filename: File to be read.
+
+        Returns:
+            Nothings
+    '''
     with open(filename, 'r') as f:
         file = f.read()
         print(file)
