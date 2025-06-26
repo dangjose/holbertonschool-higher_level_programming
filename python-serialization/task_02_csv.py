d320cc452181448d1491d21ff9f823bbd7f4e22d
dangjose <dangjos02@gmail.com>
2025-06-26 11:58:28 +1000

    remove rb and wb

--- python-serialization/task_02_csv.py
+++ python-serialization/task_02_csv.py
@@ -17,12 +17,12 @@ def convert_csv_to_json(csv_filename):
     '''
     data = []
     try:
-        with open(csv_filename, 'rb') as csv_f:
+        with open(csv_filename, 'r') as csv_f:
             reader = csv.DictReader(csv_f)
             for row in reader:
                 data.append(row)
 
-        with open('data.json', 'wb') as json_f:
+        with open('data.json', 'w') as json_f:
             json.dump(data, json_f)
 
         return True
