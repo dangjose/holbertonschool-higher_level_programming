9d4ee6a52a551f9febd332e41bcef2ba4ec357b7
dangjose <dangjos02@gmail.com>
2025-06-26 10:54:07 +1000

    task_00_basic_serialization.py

---
+++ python-serialization/task_00_basic_serialization.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Task 0. Basic Serialization"""
+
+import pickle
+
+
+def serialize_and_save_to_file(data, filename):
+    '''
+        Serialize and save data to a specified file.
+
+        Args:
+            data: Material to serialize and save.
+            filename: Name of file to save data to.
+    '''
+    with open(filename, 'w') as f:
+        pickle.dump(data)
+
+def load_and_deserialize(filename):
+    '''
+        Load and deserialize data from a specified file.
+
+        Args:
+            filename: Name of file to be deserialized.
+
+        Returns:
+            Deserialized data
+    '''
+    return pickle.load(filename)
