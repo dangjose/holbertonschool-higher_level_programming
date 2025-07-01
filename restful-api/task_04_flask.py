499467a6fc86f0a790560a196bda8aeb339db48a
dangjose <dangjos02@gmail.com>
2025-07-01 18:41:25 +1000

    pycodestyle and app.run at end of script

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -9,19 +9,21 @@ app = Flask(__name__)
 
 users = {}
 
+
 @app.route("/")
 def home():
     return "Welcome to the Flask API!"
 
-if __name__ == "__main__": app.run()
 
 @app.route("/data")
 def data():
     return jsonify(list(users.keys()))
 
+
 @app.route("/status")
 def status():
     return "OK"
+
 
 @app.route("/users/<username>")
 def show_user(username):
@@ -31,14 +33,19 @@ def show_user(username):
     else:
         return {"error": "User not found"}
 
+
 @app.route("/add_user", methods=["POST"])
 def add_user():
     data = request.get_json()
     username = data.get("username")
 
     if not username:
-        return ({"error":"Username is required"}), 400
+        return ({"error": "Username is required"}), 400
 
     users[username] = data
 
     return ({"message": "User added", "user": data}), 201
+
+
+if __name__ == "__main__":
+    app.run()
