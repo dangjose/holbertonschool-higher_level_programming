ae77d9c96a1b8f395a6ad61210e8f874e57e5807
dangjose <dangjos02@gmail.com>
2025-07-01 18:36:02 +1000

    task_04_flask.py

---
+++ restful-api/task_04_flask.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""Task 4. Develop a Simple API using Python with Flask"""
+
+from flask import Flask
+from flask import jsonify
+from flask import request
+
+app = Flask(__name__)
+
+users = {}
+
+@app.route("/")
+def home():
+    return "Welcome to the Flask API!"
+
+if __name__ == "__main__": app.run()
+
+@app.route("/data")
+def data():
+    return jsonify(list(users.keys()))
+
+@app.route("/status")
+def status():
+    return "OK"
+
+@app.route("/users/<username>")
+def show_user(username):
+    user = users.get(username)
+    if user:
+        return jsonify(user)
+    else:
+        return {"error": "User not found"}
+
+@app.route("/add_user", methods=["POST"])
+def add_user():
+    data = request.get_json()
+    username = data.get("username")
+
+    if not username:
+        return ({"error":"Username is required"}), 400
+
+    users[username] = data
+
+    return ({"message": "User added", "user": data}), 201
