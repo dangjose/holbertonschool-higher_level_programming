c65adf7485ae0c081646a90ed49e5b2ce4d280f6
dangjose <dangjos02@gmail.com>
2025-07-15 20:39:48 +1000

    5-full_table.sql

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,5 +1,2 @@
--- Creates a table called first_table in the current database in your MySQL server
-CREATE TABLE IF NOT EXISTS first_table (
-    id INT,
-    name VARCHAR(256)
-);
+-- Prints the following description of the table first_table from the database hbtn_0c_0 in your MySQL server.
+SHOW CREATE TABLE first_table;
